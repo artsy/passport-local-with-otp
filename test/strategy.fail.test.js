18d9c4f9e1e8b84c21857e9261086a2e3a9f2a3f
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-05-05 23:20:26 -0400

    Add and run Prettier

--- test/strategy.fail.test.js
+++ test/strategy.fail.test.js
@@ -1,65 +1,64 @@
 /* global describe, it, expect, before */
 /* jshint expr: true */
 
-var chai = require('chai')
-  , Strategy = require('../lib/strategy');
+var chai = require("chai"),
+  Strategy = require("../lib/strategy")
 
+describe("Strategy", function () {
+  describe("failing authentication", function () {
+    var strategy = new Strategy(function (username, password, otp, done) {
+      return done(null, false)
+    })
 
-describe('Strategy', function() {
+    var info
 
-  describe('failing authentication', function() {
-    var strategy = new Strategy(function(username, password, otp, done) {
-      return done(null, false);
-    });
-
-    var info;
-    
-    before(function(done) {
-      chai.passport(strategy)
-        .fail(function(i) {
-          info = i;
-          done();
+    before(function (done) {
+      chai
+        .passport(strategy)
+        .fail(function (i) {
+          info = i
+          done()
         })
-        .req(function(req) {
-          req.body = {};
-          req.body.username = 'johndoe';
-          req.body.password = 'secret';
-          req.body.otp = '123456';
+        .req(function (req) {
+          req.body = {}
+          req.body.username = "johndoe"
+          req.body.password = "secret"
+          req.body.otp = "123456"
         })
-        .authenticate();
-    });
-    
-    it('should fail', function() {
-      expect(info).to.be.undefined;
-    });
-  });
+        .authenticate()
+    })
 
-  describe('failing authentication with info', function() {
-    var strategy = new Strategy(function(username, password, otp, done) {
-      return done(null, false, { message: 'authentication failed' });
-    });
+    it("should fail", function () {
+      expect(info).to.be.undefined
+    })
+  })
 
-    var info;
-    
-    before(function(done) {
-      chai.passport(strategy)
-        .fail(function(i) {
-          info = i;
-          done();
+  describe("failing authentication with info", function () {
+    var strategy = new Strategy(function (username, password, otp, done) {
+      return done(null, false, { message: "authentication failed" })
+    })
+
+    var info
+
+    before(function (done) {
+      chai
+        .passport(strategy)
+        .fail(function (i) {
+          info = i
+          done()
         })
-        .req(function(req) {
-          req.body = {};
-          req.body.username = 'johndoe';
-          req.body.password = 'secret';
-          req.body.otp = '123456';
+        .req(function (req) {
+          req.body = {}
+          req.body.username = "johndoe"
+          req.body.password = "secret"
+          req.body.otp = "123456"
         })
-        .authenticate();
-    });
-    
-    it('should fail', function() {
-      expect(info).to.be.an('object');
-      expect(info.message).to.equal('authentication failed');
-    });
-  });
-  
-});
+        .authenticate()
+    })
+
+    it("should fail", function () {
+      expect(info).to.be.an("object")
+      expect(info.message).to.equal("authentication failed")
+    })
+  })
+})
--- test/strategy.options.test.js
+++ test/strategy.options.test.js
@@ -1,37 +1,37 @@
 /* global describe, it, expect, before */
 /* jshint expr: true */
 
-var chai = require('chai')
-  , Strategy = require('../lib/strategy');
+var chai = require("chai"),
+  Strategy = require("../lib/strategy")
 
+describe("Strategy", function () {
+  describe("handling a request without a body, but no username and password, with message option to authenticate", function () {
+    var strategy = new Strategy(function (username, password, otp, done) {
+      throw new Error("should not be called")
+    })
 
-describe('Strategy', function() {
+    var info, status
 
-  describe('handling a request without a body, but no username and password, with message option to authenticate', function() {
-    var strategy = new Strategy(function(username, password, otp, done) {
-      throw new Error('should not be called');
-    });
-
-    var info, status;
-    
-    before(function(done) {
-      chai.passport(strategy)
-        .fail(function(i, s) {
-          info = i;
-          status = s;
-          done();
+    before(function (done) {
+      chai
+        .passport(strategy)
+        .fail(function (i, s) {
+          info = i
+          status = s
+          done()
         })
-        .req(function(req) {
-          req.body = {};
+        .req(function (req) {
+          req.body = {}
         })
-        .authenticate({ badRequestMessage: 'Something is wrong with this request' });
-    });
-    
-    it('should fail with info and status', function() {
-      expect(info).to.be.an.object;
-      expect(info.message).to.equal('Something is wrong with this request');
-      expect(status).to.equal(400);
-    });
-  });
-  
-});
+        .authenticate({
+          badRequestMessage: "Something is wrong with this request",
+        })
+    })
+
+    it("should fail with info and status", function () {
+      expect(info).to.be.an.object
+      expect(info.message).to.equal("Something is wrong with this request")
+      expect(status).to.equal(400)
+    })
+  })
+})
