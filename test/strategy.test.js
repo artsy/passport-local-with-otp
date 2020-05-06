51fc92fbcbaf23f13f707ca301db28adfae39c39
Anandaroop Roy <anandaroop.roy+github@gmail.com>
2020-05-05 23:20:27 -0400

    Make naming consistent

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -5,13 +5,13 @@ var Strategy = require("../lib/strategy")
 describe("Strategy", function () {
   var strategy = new Strategy(function () {})
 
-  it("should be named local", function () {
+  it("should be named local-with-otp", function () {
     expect(strategy.name).to.equal("local-with-otp")
   })
 
   it("should throw if constructed without a verify callback", function () {
     expect(function () {
       var s = new Strategy()
-    }).to.throw(TypeError, "LocalStrategy requires a verify callback")
+    }).to.throw(TypeError, "LocalWithOtpStrategy requires a verify callback")
   })
 })
